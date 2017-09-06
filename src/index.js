afa0f3121b7ed937228ec36dbdcae919bc2a3148
p.shcherbakov <p.shcherbakov@rambler-co.ru>
2017-09-07 00:54:13 +0300

    remove temp components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import App from './containers/App'
-import App2 from './containers/App2'
 import PopupContainer from './components/containers/Popup'
 import TransactionsContainer from './components/containers/TransactionsList'
 import configureStore from './store/configureStore'
-
-import List from './components/presentational/RefillList';
 
 const store = configureStore()
 
@@ -16,8 +12,6 @@ render(
     <div>
       <TransactionsContainer/>
       <PopupContainer/>
-      <App />
-      <App2/>
     </div>
   </Provider>,
   document.getElementById('root')
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,7 @@
 import { combineReducers } from 'redux'
-import page from './page'
-import user from './user'
-import app2 from './forApp2';
 import popup from './popup';
 import transitions from './hisoryList'
 export default combineReducers({
-  page,
-  user,
-  app2,
   popup,
   transitions
 })
